b64b31ef66184e25b007e7a3bb2d4703e88be5a3
Keming <kz138@duke.edu>
2025-12-01 10:44:16 +0800

    Do Polish Update

--- Data/processed/data_ingestion.py
+++ Data/processed/data_ingestion.py
@@ -1,8 +1,9 @@
 import urllib.request
+#Ingesting the processed data from the cloud storage
 
 url = "https://storage.googleapis.com/mlops-keming-123/Processed_data.csv"
 local_filename = "Processed_data.csv"
 
 urllib.request.urlretrieve(url, local_filename)
 
-print("Successfully saved to:", local_filename)
+print("Successfully saved data to:", local_filename)
